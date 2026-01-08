529e3cfa35a6ec9e7d872382e388349aa8a79be6
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2026-01-08 16:18:01 +0100

    remover console.logs

--- backend/utils/deleteFiles.js
+++ backend/utils/deleteFiles.js
@@ -4,20 +4,15 @@ import path from "path";
 export const deleteFile = (imageUrl) => {
   if (!imageUrl) return;
 
-  // imageUrl = "/uploads/archivo.jpg"
   const filename = path.basename(imageUrl); // 👈 saca SOLO el nombre
   const filePath = path.join(process.cwd(), "uploads", filename);
 
   try {
     if (fs.existsSync(filePath)) {
       fs.unlinkSync(filePath);
-      console.log("🗑️ Imagen eliminada:", filePath);
     } else {
-      console.log("⚠️ Imagen no encontrada:", filePath);
     }
   } catch (err) {
     console.error("❌ Error al eliminar archivo:", err);
   }
 };
-
-
